0857e0da16a1ec99e6d9e576c47256586d925c82
GulshanJha00 <gk4763549@gmail.com>
2024-11-07 00:06:57 +0530

    page.tsx loding animation setup

--- frontend/app/loading.tsx
+++ frontend/app/loading.tsx
@@ -1,18 +1,12 @@
-
-
-
+'use client'
 const loading = () => {
     return (
         <div className="typewriter">
-  <div>
-    <h1 className="text-4xl">BUILD YOUR TECHINCAL EXPERTISE</h1>
-  </div>
-</div>
+            <div>
+                <h1 className="text-4xl">BUILD YOUR TECHINCAL EXPERTISE</h1>
+            </div>
+        </div>
     )
 }
 
 export default loading
-
-
-
-
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,23 @@
+'use client'
+
 import Image from "next/image";
+import {useEffect, useState } from "react";
+import Loading from "./loading";
+
+const Page = () => {
+
+  const [loader, setLoader] = useState(true)
+
+  useEffect(()=>{
+
+    const timer = setTimeout(() => {
+      setLoader(false);
+    }, 3000);
+  
+  },[])
+
+
+  if(loader) return <Loading/>
 
 export default function Home() {
   return (
