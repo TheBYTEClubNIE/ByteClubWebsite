44697961fe1e6989fea73ece7f1b2b318cd39255
Kaushal Prakash <kingkaushal1289@gmail.com>
2024-11-08 02:14:06 +0530

    completed leads sec

---
+++ frontend/app/components/SvgText/AnimatedText.tsx
@@ -0,0 +1,12 @@
+"use client";
+import "./index.css";
+
+const SvgText = () => (
+  <div className="translate-y-14 flex justify-center items-center font-rubic">
+    <p className="text" data-text="OUR LEADS">
+      OUR LEADS
+    </p>
+  </div>
+);
+
+export default SvgText;
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily:{
+        rubic: ['Rubic Glitch','sans-serif'],
+      }
     },
   },
   plugins: [],
