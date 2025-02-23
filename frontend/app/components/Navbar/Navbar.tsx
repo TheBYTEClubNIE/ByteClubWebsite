5c231a77afa508eec58884805cb285254459053a
devShadow <147122977+kaushal-Prakash@users.noreply.github.com>
2025-02-23 22:28:15 +0530

    debugged navbar transition gltich

--- frontend/app/components/Navbar/Navbar.tsx
+++ frontend/app/components/Navbar/Navbar.tsx
@@ -14,53 +14,57 @@ import {
 const Navbar: React.FC = () => {
   useEffect(() => {
     const handleClick = (e: MouseEvent) => {
-      const target = e.target as HTMLElement;
-      if (target.tagName === "BUTTON") {
-        const activeButton = document.querySelector("nav .active");
-        if (activeButton) {
-          activeButton.classList.remove("active");
-        }
-        target.classList.add("active");
+      const target = (e.target as HTMLElement).closest("button");
+      
+      if (!target) return;
+    
+      const activeButton = document.querySelector(".btn.active");
+      if (activeButton) {
+        activeButton.classList.remove("active");
       }
+  
+      target.classList.add("active");
     };
-
+  
     const nav = document.querySelector("nav");
     if (nav) {
       nav.addEventListener("click", handleClick);
     }
-
+  
     return () => {
       if (nav) {
         nav.removeEventListener("click", handleClick);
       }
     };
   }, []);
+  
+  
 
   return (
     <div className="flex items-center justify-center w-full h-fit z-50 absolute ">
       <nav className="navbar fixed w-max backdrop:blur-md bg-transparent mt-28 shadow-sm backdrop-blur-md">
-        <button type="button" title="Events">
+        <button type="button" title="Events" className="btn">
           <a className="material-symbols-outlined" href="#events">
             
             <MdEmojiEvents />
           </a>
         </button>
-        <button type="button" title="About">
+        <button type="button" title="About" className="btn">
           <a className="material-symbols-outlined" href="#about">
             <MdInfo />
           </a>
         </button>
-        <button type="button" className="active" title="Home">
+        <button type="button" className="active btn" title="Home">
           <a className="material-symbols-outlined" href="#home">
             <MdHome />
           </a>
         </button>
-        <button type="button" title="Review">
+        <button type="button" title="Review" className="btn">
           <a className="material-symbols-outlined" href="#review">
             <MdRateReview />
           </a>
         </button>
-        <button type="button" title="Feedback">
+        <button type="button" title="Feedback" className="btn">
           <a className="material-symbols-outlined" href="#write-review">
             <MdEmail />
           </a>
